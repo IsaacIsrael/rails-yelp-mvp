929a049d7ca0281d69ce3b281fcb2b577e005965
isaac israel <email.iisrael@gmail.com>
2019-02-13 13:25:07 -0200

    rails g controller restaurants index show new

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Restaurant Controller
+class RestaurantsController < ApplicationController
+  def index; end
+
+  def show; end
+
+  def new; end
+
+  def create; end
+end
