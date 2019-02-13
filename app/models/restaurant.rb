86f0d20e87c9930e4888ce9e41f81a15b4d35a74
isaac israel <email.iisrael@gmail.com>
2019-02-13 12:26:17 -0200

    Add validation to Restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,12 @@
 # Restaurant
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :adress, presence: true
+  validates :category, inclusion: { in: %w[chinese
+                                           italian
+                                           japanese
+                                           french
+                                           belgian] }
 end
