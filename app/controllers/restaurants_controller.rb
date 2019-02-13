60f061ecd8a837f473fd4c5652b9b7bd84c30386
isaac israel <email.iisrael@gmail.com>
2019-02-13 14:28:41 -0200

    Add feature to create new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,11 +14,24 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
-  def create; end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
 
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number,
+                                       :category)
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
+RESTAURANT_CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
 # Restaurant
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, inclusion: { in: Restaurant.category }
-
-  def self.category
-    %w[chinese italian japanese french belgian]
-  end
+  validates :category, inclusion: { in: RESTAURANT_CATEGORIES }
 end
